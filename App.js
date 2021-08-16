1b37e73ad76b0587783019b08753d9a742e7fbb0
Paulo Ricardo Busch <paulo202015@outlook.com.br>
2021-08-16 19:47:59 -0300

    feat: project files

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Teste app</Text>
+    </View>
+  );
+};
+
+export default App;
